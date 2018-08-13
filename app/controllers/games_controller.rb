65e0c5c1d7401e609d7ba2dcfa5fef7441669b50
Gonzague Gru <gonzague@agricool.co>
2018-08-13 18:52:30 +0200

    rail longuest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,39 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = (0...10).map { ('a'..'z').to_a[rand(26)] }
+  end
+
+  def score
+    @letters = params[:new].split(',')
+    @view = score_and_message(params[:score], @letters)
+    # puts params[:score].length
+  end
+
+  private
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def score_and_message(attempt, grid)
+    if included?(attempt, grid)
+      if english_word?(attempt)
+        score = attempt.length
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
